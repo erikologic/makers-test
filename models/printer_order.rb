07f54d4b21a96887bb80b9fc30e21a1dd6a85902
Enrico Graziani <mrenrich84@gmail.com>
2017-05-12 00:33:06 +0100

    PrinterOrder: minor refactoring: splitting output in a series of private methods for readability

--- models/printer_order.rb
+++ models/printer_order.rb
@@ -9,33 +9,65 @@ class PrinterOrder
     empty_cart: "Your cart is empty, please add some items"
   }.freeze
 
+  attr_accessor :order
+
   def output(order)
-    return MESSAGES[:empty_cart] if order.items.count == 0
+    return empty_cart_msg if order.items.count == 0
 
+    self.order = order
     [].tap do |result|
-      result << "Order for #{order.material.identifier}:"
-
-      result << COLUMNS.map { |name, width| name.to_s.ljust(width) }.join(' | ')
+      result << output_material
+      result << output_header
       result << output_separator
-
-      order.items.each_with_index do |(broadcaster, delivery), index|
-        result << [
-          broadcaster.name.ljust(COLUMNS[:broadcaster]),
-          delivery.name.to_s.ljust(COLUMNS[:delivery]),
-          ("$#{delivery.price}").ljust(COLUMNS[:price])
-        ].join(' | ')
-      end
-
+      result << output_body
       result << output_separator
-      result << "Total: $#{order.items_cost}"
-      result << if order.discount
-        ["Promotions applied: $#{order.get_discount}",
-        "Grand total: $#{order.total_cost}"]
-      end
+      result << output_items_total_and_discount if order.discount
+      result << output_grand_total
     end.join("\n")
   end
 
   private
+
+  def empty_cart_msg
+    MESSAGES[:empty_cart]
+  end
+
+  def output_material
+    "Order for #{order.material.identifier}:"
+  end
+
+  def output_header
+    COLUMNS.map { |name, width| name.to_s.ljust(width) }.join(' | ')
+  end
+
+  def output_body
+    order.items.map do |broadcaster, delivery|
+      [
+        broadcaster.name.ljust(COLUMNS[:broadcaster]),
+        delivery.name.to_s.ljust(COLUMNS[:delivery]),
+        ("$#{delivery.price}").ljust(COLUMNS[:price])
+      ].join(' | ')
+    end
+  end
+
+  def output_items_total_and_discount
+    [
+      output_items_total,
+      output_discount,
+    ]
+  end
+
+  def output_items_total
+    "Total: $#{order.items_cost}"
+  end
+
+  def output_discount
+    "Promotions applied: $#{order.get_discount}"
+  end
+
+  def output_grand_total
+    "Grand total: $#{order.total_cost}"
+  end
 
   def output_separator
     @output_separator ||= COLUMNS.map { |_, width| '-' * width }.join(' | ')
